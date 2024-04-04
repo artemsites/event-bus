9c85833b2bf95784ddc1cbeed034c0313f69f2b6
artemsites <info@artemsites.ru>
2024-04-04 19:21:24 +0400

    es5 > es6

--- eventBus.js
+++ eventBus.js
@@ -1,49 +1,48 @@
-function EventBus() {
-  this.listeners = {}
-  this.queue = []
-}
-
-EventBus.prototype.on = function (event, callback) {
-  this.listeners[event] = this.listeners[event] || []
-  this.listeners[event].push(callback)
-
-  this.queue.forEach(function (queuedEvent) {
-    if (queuedEvent.event === event) {
-      setTimeout(function () {
-        callback.apply(null, queuedEvent.args)
-      }, 0)
-    }
-  })
-  this.queue = this.queue.filter(function (queuedEvent) {
-    return queuedEvent.event !== event
-  })
-}
-
-EventBus.prototype.off = function (event, callback) {
-  if (!this.listeners[event]) {
-    return
+class EventBus {
+  constructor() {
+    this.listeners = {}
+    this.queue = []
   }
-  this.listeners[event] = this.listeners[event].filter(function (listener) {
-    return listener !== callback
-  })
-  if (this.listeners[event].length === 0) {
-    delete this.listeners[event]
-  }
-}
 
-EventBus.prototype.emit = function (event) {
-  var args = Array.prototype.slice.call(arguments, 1)
-  if (this.listeners[event]) {
-    this.listeners[event].forEach(function (callback) {
-      setTimeout(function () {
-        callback.apply(null, args)
-      }, 0)
+  on(event, callback) {
+    this.listeners[event] = this.listeners[event] || []
+    this.listeners[event].push(callback)
+
+    this.queue.forEach((queuedEvent) => {
+      if (queuedEvent.event === event) {
+        setTimeout(() => {
+          callback(...queuedEvent.args)
+        }, 0)
+      }
     })
-  } else {
-    this.queue.push({ event: event, args: args })
+    this.queue = this.queue.filter((queuedEvent) => queuedEvent.event !== event)
+  }
+
+  off(event, callback) {
+    if (!this.listeners[event]) {
+      return
+    }
+    this.listeners[event] = this.listeners[event].filter(
+      (listener) => listener !== callback
+    )
+    if (this.listeners[event].length === 0) {
+      delete this.listeners[event]
+    }
+  }
+
+  emit(event, ...args) {
+    if (this.listeners[event]) {
+      this.listeners[event].forEach((callback) => {
+        setTimeout(() => {
+          callback(...args)
+        }, 0)
+      })
+    } else {
+      this.queue.push({ event: event, args })
+    }
   }
 }
 
-var eventBus = new EventBus()
+const eventBus = new EventBus()
 
 export default eventBus
